9ed5251a5359b65405b9c4aa69a4d386ce6e04b5
Anup Pradhan <anuppradhan929@gmail.com>
2025-07-09 21:18:45 +0530

    feat: add debug logging for MongoDB connection and update connection method

--- src/db/connect.db.js
+++ src/db/connect.db.js
@@ -1,10 +1,16 @@
 
 import mongoose from "mongoose";
+import debug from "debug";
+import dotenv from "dotenv";
+
+
+dotenv.config({path: "./.env"});
+const debgr = debug('Dev:DBConnect');
 
 mongoose
-.connect("mongodb://localhost:27017/ecommerce")
-.then(() => {console.log("Connected to MongoDB successfully")})
-.catch((err) => {console.error("Error connecting to MongoDB:", err)});
+.connect(process.env.MONGO_URI)
+.then(() => {debgr("Connected to MongoDB successfully")})
+.catch((err) => {debgr("Error connecting to MongoDB:", err)});
 
 const db = mongoose.connection;
 export default db;
